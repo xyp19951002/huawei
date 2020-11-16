19a43c96aee81648506070037c24c6371cfd59f1
xyp19951002 <2372245528@qq.com>
2020-11-16 17:23:24 +0800

    v3.0.1

--- public/js/login.js
+++ public/js/login.js
@@ -4,50 +4,51 @@ import 'https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js';
 import { baseUrl } from './library/config.js';
 import cookie from './library/cookie.js';
 
-// $(function() {
-//     let phoneReg = /^1[3-9]\d{9}/;
-//     let pasdReg = /^[0-9a-zA-Z]{6,16}$/;
-//     let email = /^[A-z]\w{5,15}@[A-z0-9-]{2,}\.[A-z]{2,7}\.?[A-z]*$/;
+// 表单验证
+$(function() {
+    let phReg = /^1[3-9]\d{9}/; //手机正则
+    let pasReg = /^[0-9a-zA-Z]{6,16}$/; // 密码正则
+    let email = /^[A-z]\w{5,15}@[A-z0-9-]{2,}\.[A-z]{2,7}\.?[A-z]*$/; // 邮件正则
 
-//     $('.hwid-pwdlogin-root').on('input', function(e) {
-//         let target = e.target;
-//         if (target.name === 'phone') {
-//             $('.phoneinfo').empty();
-//             if(phoneReg.test($('[name=phone]').val())) {
-//                 $('.phoneinfo').append(`<span>*&nbsp;&nbsp;手机号码输入正确</span>`);
-//             } else {
-//                 $('.phoneinfo').append(`<span style="color:red">*&nbsp;&nbsp;输入错误</span>`);
-//             };
-//         } else if (target.name === 'password') {
-//             $('.psdinfo').empty();
-//             if (pasdReg.test($('[name=password]').val())) {
-//                 $('.psdinfo').append('<span></span>');
-//             } else {
-//                 $('.psdinfo').append('<span style="color:red">密码输入错误</span>');
-//             }
-//         }
-//     })
-// });
+    $('[type=text]').on('input', function(e) {
+        let target = e.target;
+        if (target.type === 'text') {
+            // 表单验证提示文字 清空
+            $('.phtext').empty();
+            if(phReg.test($('[type=text]').val())) {
+                $('.phtext').append(`<span>手机号可登录</span>`);
+            } else {
+                $('.phtext').append(`<span style="color:red">用户名不存在</span>`);
+            };
+        } else if (target.type === 'password') {
+            $('.pastext').empty();
+            if (pasReg.test($('[type=password]').val())) {
+                $('.pastext').append('<span></span>');
+            } else {
+                $('.pastext').append('<span style="color:red">密码输入错误</span>');
+            }
+        }
+    })
+});
 
-// $('.login-btn').on('click', function() {
-//     let password = $.md5($('[name=password]').val());
+$('[type=button]').on('click', function() {
+    let password = $.md5($('[type=password]').val());
 
-//     $.ajax({
-//         type: "post",
-//         url: `${baseUrl}/users/login`,
-//         data: {
-//             password: password,
-//             phone: $('[name=phone]').val(),
-//         },
-//         dataType: "json",
-//         success: function(res) {
-//             console.log(res);
-//             res.forEach(item => {
-//                 if (String(item.user_phone) === $('[name=phone]').val()) {
-//                    alert('登录成功');
-//                    window.location.href = '/';
-//                 }
-//             })
-//         }
-//     });
-// });
+    $.ajax({
+        type: "post",
+        url: `${baseUrl}/users/login`,
+        data: {
+            password: password,
+            phone: $('[type=text]').val(),
+        },
+        dataType: "json",
+        success: function(res) {
+            res.forEach(item => {
+                if (String(item.user_phone) === $('[type=text]').val()) {
+                   alert('登录成功');
+                   window.location.href = '/';
+                }
+            })
+        }
+    });
+});
